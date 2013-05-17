975de9509c94c3322df00f5180597ff67975f8b2
dmelian <domingo@meliangonzalez.com>
2013-05-17 17:17:13 +0100

    alternative folder

---
+++ source/alt/frm/paper.php
@@ -0,0 +1,10 @@
+<?php
+class {
+	public $contents=array();
+	
+	public function setLogo($logo){ 
+		$this->contents['.ia_logo']= "<img id='ia_img_logo' src='{$this->logo}' height='150px' border='0' >";
+	} 
+		
+}
+	
