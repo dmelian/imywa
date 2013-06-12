fc3db28a9e6fac2d99002d2859d0890777e32d77
dmelian <domingo@meliangonzalez.com>
2013-06-12 17:03:22 +0100

    updates

--- source/test/frmx/cardframe/form.php
+++ source/test/frmx/cardframe/form.php
@@ -6,20 +6,7 @@ class test_frmx_cardframe_form extends bas_frmx_form{
 		parent::OnLoad();
 		$this->toolbar= new bas_frmx_toolbar('pdf,csv,config,close');
 		$this->title= 'Card Frame Test';
-// 		$this->loadFrame('test_frmx_cardframe_form_frame1');
-// 		$this->loadFrame('test_frmx_cardframe_form_frame2');
-
-
-
-
-
-		//$this->addFrame(new test_frmx_cardframe_form_frame1('frame1','Testing Card Frame 1'));
-		//$this->addFrame(new test_frmx_cardframe_form_frame1('frame1','Testing Card Frame 2-0'));
-		//$this->addFrame(new test_frmx_cardframe_form_frame2('frame2','Testing Card Frame 2'));
-	/*	$this->addFrame(new test_frmx_cardframe_form_frame1('frame1','Testing Card Frame 1-2'));
-		$this->addFrame(new test_frmx_cardframe_form_frame1('frame1','Testing Card Frame 1-3'));
-		$this->addFrame(new test_frmx_cardframe_form_frame3('frame3','Testing Card Frame 3-0'));
-		$this->addFrame(new test_frmx_cardframe_form_frame4('frame4','Testing Card Frame 4-0'));*/
+		
 		$this->buttonbar = new bas_frmx_buttonbar();
 		$this->buttonbar->addAction("aceptar");
 		$this->buttonbar->addAction("eliminar");
@@ -41,7 +28,9 @@ class test_frmx_cardframe_form extends bas_frmx_form{
 // 		$this->buttonbar->addMenu("eliminar",array("primero","segundo","tercero"));
 // 		$this->buttonbar->addMenu("eliminar > primero",array("A","B","C"));
         $card = new bas_frmx_gridFrame("grid",array("grid"));
+        
         $grid = new bas_frmx_panelGridQuery("pepe");
+        
 //         $gid->addComponent(1,1,"AMIGO");
         $grid->query->add("grid","test");
         $grid->query->addcol("id","ID","grid",true,"test");
