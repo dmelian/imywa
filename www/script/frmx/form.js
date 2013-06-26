ced90e273a1a1d172c237f17d89f12272596abc1
inmaglez <inma.gonzalezcruz@gmail.com>
2013-06-26 12:55:56 +0100

    acceso

--- www/script/frmx/form.js
+++ www/script/frmx/form.js
@@ -363,7 +363,7 @@ bas_frmx_form.prototype.selectedItems= function(){
 
 bas_frmx_form.prototype.sendEclive= function(){
 	$("#eclive_cups").attr("value",currentForm.frames["cups"].getSelectedField("cups"));
-	$("#eclive_form").click();
+	$("#eclive_cups").click();
 };
 
 // -------------------------------------------------
